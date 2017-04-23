a8268eb70516c811380f71984e89a49399f2b7b3
panzhiev <panzhiev.timur@gmail.com>
2017-04-23 22:15:10 +0300

    3

---
+++ app/src/main/java/com/tim/retrofittranslator/TranslationConstants.java
@@ -0,0 +1,19 @@
+package com.tim.retrofittranslator;
+
+/**
+ * Created by Tim on 23.04.2017.
+ */
+
+public class TranslationConstants {
+
+    public static final String ruEn = "ru-en";
+    public static final String enRu = "en-ru";
+    public static final String ruDe = "ru-de";
+    public static final String deRu = "de-ru";
+    public static final String ruFr = "ru-fr";
+    public static final String frRu = "fr-ru";
+    public static final String ruPl = "ru-pl";
+    public static final String plRu = "pl-ru";
+    public static final String ruZh = "ru-zh";
+    public static final String zhRu = "zh-ru";
+}
